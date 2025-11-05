edb3f38a346b4ba4080e171cf89f616102f78491
Zätha Malmberg <zathamalmberg@gmail.com>
2025-11-05 19:20:21 +0100

    fixed the error

--- Assets/Scrips/SetPrefab.cs
+++ Assets/Scrips/SetPrefab.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class SetPrefab : MonoBehaviour
 {
-    public XRReferenceImageLibrary Lib;
+    //public XRReferenceImageLibrary Lib;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
