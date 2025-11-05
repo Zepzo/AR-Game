8f14d2bf519f8c23444836089ea882c32e0d1aee
Zätha <zathamalmberg@gmail.com>
2025-11-05 14:49:39 +0100

    Started working on assignment

---
+++ Assets/Scrips/SetPrefab.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SetPrefab : MonoBehaviour
+{
+    public XRReferenceImageLibrary Lib;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
